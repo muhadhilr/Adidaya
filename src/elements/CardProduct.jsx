00190cf72a1c5a72d598095d775288119839186d
Muhammad Fadhiil <fadhilrmdhn3010@gmail.com>
2024-03-20 01:39:02 +0700

    fix: Fix layout on ProductPage and add hooks to ProductPage

--- src/elements/CardProduct.jsx
+++ src/elements/CardProduct.jsx
@@ -2,22 +2,30 @@ import React from "react";
 import ProductImage from "../assets/images/product-1.png";
 import Arrow from "../assets/icon/rightArrow.png";
 import { Link } from "react-router-dom";
+import { IoMdArrowForward } from "react-icons/io";
 
-const CardProduct = () => {
+const CardProduct = ({ title, image, children, link }) => {
   return (
-    <div className="flex flex-col border border-[#B9B6B3] w-[30%] rounded-3xl p-7 hover:bg-green hover:text-white m-2">
-      <div className="w-full">
-        <img src={ProductImage} alt="" className="w-full" />
+    <div className="flex flex-col border border-[#B9B6B3] md:w-[30%] h-[30rem] rounded-3xl p-7 hover:bg-green hover:text-white m-3 shadow-md">
+      <div className="w-full h-52">
+        <img
+          src={image}
+          alt=""
+          className="w-full h-full object-cover rounded-xl"
+        />
       </div>
-      <div>
-        <h1 className="font-bold text-h3 my-3 hover:text-white">Bebek Abas</h1>
-        <p className="mb-6 text-justify">
-          Lorem ipsum dolor sit amet consecte tur adipiscing elit semper
-          dalaracc lacus vel facilisis volutpat est velitolm.
-        </p>
-        <Link to="/" className="flex items-center font-bold">
-          Selengkapnya <img src={Arrow} alt="" className="ml-2" />
-        </Link>
+      <div className="flex flex-col justify-between">
+        <div>
+          <h1 className="font-bold text-[27px] my-3 whitespace-nowrap overflow-hidden">{title}</h1>
+          <p className="mb-6 text-justify pr-2">
+            {children}
+          </p>
+        </div>
+        <div className="mb-3">
+          <Link to={link} className="flex items-center font-bold py-2">
+            Selengkapnya <IoMdArrowForward className="ml-2"/>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,28 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import CardProduct from "../elements/CardProduct";
+import BackgroundImage from "../assets/images/background/bgProductpage.png";
+import withNavbar from "../hoc/withNavbar";
 
 const ProductPage = () => {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      image: "https://via.placeholder.com/150",
+      name: "Bebek Abas",
+      description: "Tempat makan yang menyajikan bebek goreng dan bakar. Terletak di Jl. Kusuma Wijaya 24, Peru, Semarang",
+    },
+    {
+      id: 2,
+      image: "https://via.placeholder.com/150",
+      name: "Mie Rahmad",
+      description: "Usaha bisnis yang menawarkan berbagai varian mie dengan beragam pilihan kuah. bertempat di Jl. Sukun 23, Tunggul, Jogja",
+    },
+    {
+      id: 3,
+      image: "https://via.placeholder.com/150",
+      name: "KerajinanQu",
+      description: "Usaha bisnis yang berfokus pada produksi dan penjualan kerajinan tangan, bertekmpat di Jl. Mojolangu 2, Merjosari",
+    },
+    {
+      id: 4,
+      image: "https://via.placeholder.com/150",
+      name: "Kelontong Tomo",
+      description: "Bisnis toko kelontong yang menyediakan berbagai macam kebutuhan sehari-hari masyarakat. Terletak di Jl. Agung 2, Klojen",
+    },
+    {
+      id: 5,
+      image: "https://via.placeholder.com/150",
+      name: "Kios Tlogomas",
+      description: "Kios Tlogomas berfokus pada jual-beli buku bekas dengan harga terjangkau, bertempat di Jl. Ahmad 2, Singosari",
+    },
+    {
+      id: 6,
+      image: "https://via.placeholder.com/150",
+      name: "Mie Ayam Cak Qodir",
+      description: "Mie Ayam Cak Qodir adalah warung mie ayam yang didirikan tahun 2023 dan bertempat di Jl. Soekarno Hatta 23",
+    },
+  ]);
+
   return (
-    <div className="p-12">
-      <div className="flex flex-col justify-center items-center text-center">
-        <div className="w-1/2">
-          <h1 className="text-h1 font-bold">Daftar UMKM</h1>
-          <p className="text-grey font-medium">
-            Lorem ipsum dolor sit amet consectetur adipiscing elit semper dalar
-            elementum tempus hac tellus libero accumsan.
-          </p>
-        </div>
-        <div className="w-full text-justify flex flex-wrap justify-center my-8">
-          <CardProduct />
-          <CardProduct />
-          <CardProduct />
-          <CardProduct />
-          <CardProduct />
-          <CardProduct />
+    <div
+      className="flex justify-center"
+      style={{
+        backgroundImage: `url(${BackgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="mt-20 md:w-11/12 md:py-20">
+        <div className="flex flex-col justify-center items-center text-center px-5">
+          <div className="md:w-1/2">
+            <h1 className="text-h2 md:text-h1 font-bold">Daftar UMKM</h1>
+            <p className="text-grey md:font-medium">
+              Lorem ipsum dolor sit amet consectetur adipiscing elit semper
+              dalar elementum tempus hac tellus libero accumsan.
+            </p>
+          </div>
+          <div className="w-full text-justify flex flex-col md:flex-row flex-wrap justify-center my-8">
+            {data.map((item) => (
+              <CardProduct
+                key={item.id}
+                image={item.image}
+                title={item.name}
+                children={item.description}
+                link={`/product/${item.id}`}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default ProductPage;
+export default withNavbar(ProductPage);
